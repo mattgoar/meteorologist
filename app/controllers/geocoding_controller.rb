1fc3a8c4dd23fd7be41ad8c9d79f9fe359a60269
mattgoar <mattgoar@gmail.com>
2015-07-17 11:33:12 -0500

    Added error checking for blank string input

--- app/controllers/geocoding_controller.rb
+++ app/controllers/geocoding_controller.rb
@@ -9,24 +9,31 @@ class GeocodingController < ApplicationController
 
   def street_to_coords
     @street_address = params[:user_street_address]
-    url_safe_street_address = URI.encode(@street_address)
 
-    # ==========================================================================
-    # Your code goes below.
-    # The street address the user input is in the string @street_address.
-    # A URL-safe version of the street address, with spaces and other illegal
-    #   characters removed, is in the string url_safe_street_address.
-    # ==========================================================================
 
-    url = open('https://maps.googleapis.com/maps/api/geocode/json?address='+url_safe_street_address)
-    raw_data = url.read
+    if @street_address == ""
+      render :file => 'public/500.html', :status => 500, :layout => false
+    else
 
-    parsed_data = JSON.parse(raw_data)
+      url_safe_street_address = URI.encode(@street_address)
 
-    @latitude = parsed_data["results"][0]["geometry"]["location"]["lat"]
+      # ==========================================================================
+      # Your code goes below.
+      # The street address the user input is in the string @street_address.
+      # A URL-safe version of the street address, with spaces and other illegal
+      #   characters removed, is in the string url_safe_street_address.
+      # ==========================================================================
 
-    @longitude = parsed_data["results"][0]["geometry"]["location"]["lng"]
+      url = open('https://maps.googleapis.com/maps/api/geocode/json?address='+url_safe_street_address)
+      raw_data = url.read
 
-    render("street_to_coords.html.erb")
+      parsed_data = JSON.parse(raw_data)
+
+      @latitude = parsed_data["results"][0]["geometry"]["location"]["lat"]
+
+      @longitude = parsed_data["results"][0]["geometry"]["location"]["lng"]
+
+      render("street_to_coords.html.erb")
+    end
   end
 end
